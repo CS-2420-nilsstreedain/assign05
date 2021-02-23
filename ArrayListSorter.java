95c25a19263f82a7c0ace56467b8f72fe411fccf
PaulNuffer <78831963+PaulNuffer@users.noreply.github.com>
2021-02-22 22:34:43 -0700

    Methods to generate specific types of Integer arrays wrtitten

--- ArrayListSorter.java
+++ ArrayListSorter.java
@@ -1,6 +1,7 @@
 package assign05;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import assign04.OutOfRangeException;
 
@@ -37,35 +38,48 @@ public class ArrayListSorter {
 	}
 	
 	/**
-	 * Add Description here
+	 * This method generates an ArrayList of type Integer with elements beginning at
+	 * 0, and increasing in value by 1 for each element added until the ArrayList has size elements.
 	 * 
-	 * @param parameter - parameter description
-	 * @throws exception - description for why exception was thrown
-	 * @return return description
+	 * @param size, the amount of elements to be in the generated ArrayList
+	 * @return generatedAscendingArray, an array of ascending Integers. Returns an empty ArrayList
+	 * if size < 1.
 	 */	
 	public static ArrayList<Integer> generateAscending(int size) {
-		
+		ArrayList<Integer> generatedAscendingArray = new ArrayList<>();
+		for (int i = 0; i < size; i++) 
+			generatedAscendingArray.add(i);
+		return generatedAscendingArray;
 	}
 	
 	/**
-	 * Add Description here
+	 * This method generates an ArrayList of type Integer with a specific number of unique elements,
+	 * shuffled randomly using Collectoins.shuffle
 	 * 
-	 * @param parameter - parameter description
-	 * @throws exception - description for why exception was thrown
-	 * @return return description
-	 */
+	 * @param size, the amount of elements to be in the generated ArrayList
+	 * @return arrayToShuffle, an array of shuffled Integers. Returns an empty ArrayList
+	 * if size < 1.
+	 */	
 	public static ArrayList<Integer> generatePermuted(int size) {
-		
+		ArrayList<Integer> arrayToShuffle = new ArrayList<>();
+		for (int i = 0; i < size; i++) 
+			arrayToShuffle.add(i);
+		Collections.shuffle(arrayToShuffle);
+		return arrayToShuffle;
 	}
 	
 	/**
-	 * Add Description here
+	 * This method generates an ArrayList of type Integer with elements beginning at
+	 * value equal to size, and decreasing in value by 1 for each element added until the ArrayList has size elements.
 	 * 
-	 * @param parameter - parameter description
-	 * @throws exception - description for why exception was thrown
-	 * @return return description
-	 */
+	 * @param size, the amount of elements to be in the generated ArrayList
+	 * @return generatedDescendingArray, an array of descending Integers. Returns an empty ArrayList
+	 * if size < 1.
+	 */	
 	public static ArrayList<Integer> generateDescending(int size) {
-		
+		ArrayList<Integer> generatedDescendingArray = new ArrayList<>();
+		for (int i = size; i > 0; i--) 
+			generatedDescendingArray.add(i);
+		return generatedDescendingArray;
 	}
 }
