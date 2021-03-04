1399c2c8aaefb3cd6de2216d815215803aecb6ee
Nils Streedain <business@nilsstreedain.com>
2021-03-03 20:30:20 -0700

    Create ArrayListSorterTimer.java

---
+++ ArrayListSorterTimer.java
@@ -0,0 +1,19 @@
+package assign05;
+
+import java.util.Random;
+
+/**
+* This class collects running times for methods of ArrayListSorter.
+* 
+* @author Paul Nuffer, Nils Streedain
+* @version February 9, 2021
+*/
+public class ArrayListSorterTimer {
+
+	public static void main(String[] args) {
+		int seed = 325784038;
+		Random rng = new Random(seed);
+		
+
+	}
+}
